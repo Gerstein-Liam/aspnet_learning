d21bfd38f1b66d976000c5a6c276909be4fff042
DESKTOP-E3R823T\liamg <liam.gerstein@gmail.com>
2017-04-14 13:08:13 +0200

    Ajout de l'affichage du parametre dans la vue grâce a une clé/valeur. Ajout de viewdata dans controller et vue

--- openclass_helloworld/openclass_helloworld/Controllers/HomeController.cs
+++ openclass_helloworld/openclass_helloworld/Controllers/HomeController.cs
@@ -8,9 +8,21 @@ namespace openclass_helloworld.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string id)
         {
-            return View();          // Equivaut à : return View("Index");      
+
+
+            if (string.IsNullOrWhiteSpace(id))
+                return View("Error");
+            else
+                ViewData["Nom"] = id;
+                return View();           // Equivaut à : return View("Index");
+                                         // La méthode View va automatiquement chercher la vue qui placé dans 
+                                         // Le repértoire Views dans le dossier DU MEME NOM que le controller ET
+                                         // qui s'appelle comme la méthode l'action appellé
+
+
+                     
         }
     }
 }
